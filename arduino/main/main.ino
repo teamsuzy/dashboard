30c4b2255c6088ece62621472568c0bb7d938d8d
Sj3rd <sjoerdabolten@gmail.com>
2019-11-27 00:41:13 +0100

    Updated arduino

--- arduino/main/main.ino
+++ arduino/main/main.ino
@@ -25,78 +25,54 @@
 
 #define PMTK_Q_RELEASE "$PMTK605*31"
 
+SoftwareSerial rfSerial(2, 3);
+
 Adafruit_BMP280 bmp(BMP_CS);
+
 SoftwareSerial gpsSerial(8, 9);
 Adafruit_GPS GPS(&gpsSerial);
 
-String msg;
-int counter = 0;
 
 void setup() {
-    Serial.begin(9600);
+    Serial.begin(115200);
+    rfSerial.begin(9600);
     gpsSerial.begin(9600);
     gpsSerial.println(PMTK_Q_RELEASE);
     gpsSerial.println(PMTK_SET_NMEA_OUTPUT_RMCGGA);
     gpsSerial.println(PMTK_SET_NMEA_UPDATE_1HZ);
     if (!bmp.begin()) {  
-      Serial.println("Could not find a valid BMP280 sensor, check wiring!");
+      Serial.println("No BMP280 connect lol");
       while(1);
     }
     delay(1000);
-//    delay(150);
 }
 
 uint32_t timer = millis();
 void loop() {
   char c = GPS.read();
-    if (GPS.newNMEAreceived()) {
-    // a tricky thing here is if we print the NMEA sentence, or data
-    // we end up not listening and catching other sentences!
-    // so be very wary if using OUTPUT_ALLDATA and trytng to print out data
-    //Serial.println(GPS.lastNMEA());   // this also sets the newNMEAreceived() flag to false
-    if (!GPS.parse(GPS.lastNMEA()))   // this also sets the newNMEAreceived() flag to false
-      return;  // we can fail to parse a sentence in which case we should just wait for another
+  if (GPS.newNMEAreceived()) {
+    if (!GPS.parse(GPS.lastNMEA()))
+      return;
   }
-//  counter++;
-//  String temp = String(bmp.readTemperature());
-//  String alt = String(bmp.readAltitude(1004));
-//  String pres = String(bmp.readPressure());
-//  if(Serial.available() > 0){
-//    msg = Serial.readString();
-//    if(msg == '1') {
-//      digitalWrite(LED_BUILTIN, HIGH);
-//      Serial.println("{\"led\":1, \"team\": \"suzy\"}");
-//    } else if(msg == '2') {
-//      digitalWrite(LED_BUILTIN, LOW);
-//      Serial.println("{\"led\":0}");
-//    } else if(msg == '3') {
-//      Serial.println("{\"temperature\":" + temp + ", \"team\": \"suzy\"}");
-//    } else if(msg == '4') {
-//      Serial.println();
-//      Serial.println("{\"altitude\":" + alt + ", \"team\": \"suzy\"}");
-//    } else if(msg == '5') {
-//      Serial.println("{\"pressure\":" + pres + ", \"team\": \"suzy\"}");
-////    } else if(Number(msg) <= 180 && msg >= 0) {/
-////      myServo.write(msg);
-////      Serial.println("{\"servo\":" + String(msg) + ", \"team\": \"suzy\"}");
-//    } else {
-//      Serial.println("{\"msg\":\"" + msg + "\", \"team\": \"suzy\"}");
-//    }
-//  }
-if (timer > millis())  timer = millis();
-  if (millis() - timer > 2000) {
+  if (timer > millis()) { timer = millis(); }
+  if (millis() - timer > 500) {
     timer = millis(); // reset the timer
     String temp = String(bmp.readTemperature());
     String alt = String(bmp.readAltitude(1004));
     String pres = String(bmp.readPressure());
-    String gps = String(GPS.hour) + String(GPS.minute) + String(GPS.seconds);
-    String json = ("{\"alive\":" + String(millis()) + ",\"temperature\":" + temp + ",\"altitude\":" + alt + ",\"pressure\":" + pres + ", \"gps\":" + gps + ", \"team\": \"suzy\"}");
-    Serial.println(json);
-//    counter = 0;
+    if (GPS.fix) {
+      String lat = String(GPS.latitude);
+      String longitude = String(GPS.longitude);
+      String altitude = String(GPS.altitude);
+      String satellites = String(GPS.satellites);
+      String angle = String(GPS.angle);
+      String speedy = String(GPS.speed);
+      Serial.println(("{\"alive\":" +  String(millis()) + ",\"temperature\":" + temp + ",\"altitude\":" + alt + ",\"pressure\":" + pres + ",\"gps\":{\"lat\":" + lat + ",\"long\":" + longitude + ",\"altitude\":" + altitude + ",\"satellites\":" + satellites + ",\"angle\":" + angle + ",\"speed\":" + speedy + "},\"team\":\"suzy\"}"));
+      rfSerial.println(("{\"alive\":" +  String(millis()) + ",\"temperature\":" + temp + ",\"altitude\":" + alt + ",\"pressure\":" + pres + ",\"gps\":{\"lat\":" + lat + ",\"long\":" + longitude + ",\"altitude\":" + altitude + ",\"satellites\":" + satellites + ",\"angle\":" + angle + ",\"speed\":" + speedy + "},\"team\":\"suzy\"}"));
+    } else {
+      String json = ("{\"alive\":" +  String(millis()) + ",\"temperature\":" + temp + ",\"altitude\":" + alt + ",\"pressure\":" + pres + ",\"gps\":false,\"team\":\"suzy\"}");
+      Serial.println(json);
+      rfSerial.println(json);
+    }
   }
-//  delay(20);
-//  if (gpsSerial.available()) {
-//    char c = gpsSerial.read();
-//    Serial.write(c);
-//  }
 }
